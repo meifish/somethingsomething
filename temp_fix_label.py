9d5ce4bbc43c5383fd4031830db36694a5dfedda
Kathryn (meiyu) <meifish.kat@gmail.com>
2021-04-03 21:15:59 -0400

    Delete temp_fix_label.py

--- temp_fix_label.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('./toy_dataset/compositional/labels.json', 'r') as labels:
-    with open('./toy_dataset/compositional/labels_new.json', 'w') as new_labels:
-        labels = json.load(labels)
-        ascending_labels = {action : i+1 for (i, action) in enumerate(labels)}
-        json.dump(ascending_labels, new_labels)
